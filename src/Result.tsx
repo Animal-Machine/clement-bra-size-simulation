b62664fe7063f1f3ce9d9fa0142eba0f300d9bd1
Animal-Machine <cyrille.berne@protonmail.com>
2024-05-16 04:32:21 +0200

    New text in error message

--- src/Result.tsx
+++ src/Result.tsx
@@ -22,7 +22,18 @@ export const Result: React.FunctionComponent<ResultProps> = ({
   if (hasError) {
     return (
       <div className={`${BLOCK} ${BLOCK}--is-error ${className}`}>
-        Désolé, mais cette valeur n'est pas encore proposée par la marque.
+        <p>
+          Données incorrectes :
+          <br />
+          reprenez vos mesures.
+        </p>
+        <p> + de 441 tailles : du 85 au 120 et du bonnet A au G </p>
+        <p>
+          Un doute sur votre taille ?
+          <br />
+          Contacter&nbsp;
+          <a href="mailto:asymetrio.fr@gmail.com">asymetrio.fr@gmail.com</a>.
+        </p>
       </div>
     );
   }
